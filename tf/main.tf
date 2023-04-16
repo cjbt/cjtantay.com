182ae5ebcabe1348c7bfaccfae6ed1f8e938c197
Cecil tantay <cjbtantay@gmail.com>
2023-04-16 04:19:51 -0700

    apply block policy

--- tf/main.tf
+++ tf/main.tf
@@ -52,6 +52,15 @@ resource "aws_s3_bucket_policy" "allow_access_from_cloudfront" {
   })
 }
 
+resource "aws_s3_bucket_public_access_block" "sub_domain" {
+  bucket = aws_s3_bucket.sub_domain.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
 ###
 
 resource "aws_s3_bucket" "root_domain" {
