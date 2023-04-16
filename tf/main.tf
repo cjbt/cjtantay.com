4d42ef71d59067770f35565bf6c4e046af9ce2bc
Cecil tantay <cjbtantay@gmail.com>
2023-04-16 10:16:06 -0700

    attempt to fix

--- tf/main.tf
+++ tf/main.tf
@@ -1,3 +1,23 @@
+terraform {
+  cloud {
+    # The name of your Terraform Cloud organization.
+    organization = "cjbt-organization"
+
+    # The name of the Terraform Cloud workspace to store Terraform state files in.
+    workspaces {
+      name = "cjtantay.com"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+
+    }
+  }
+}
+
 provider "aws" {
   region     = "us-east-1"
 
