5334ce8e498d33349b61a0ff9d8870a7f0de1f9d
Cecil Tantay <cjbtantay@gmail.com>
2023-04-16 01:50:50 -0700

    update bucket name

--- tf/main.tf
+++ tf/main.tf
@@ -1,20 +1,16 @@
 provider "aws" {
-  region = "us-east-1"
+  region     = "us-east-1"
 
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
 }
 
 locals {
-  bucket_name = "cjtantay-dot-com-bucket"
+  bucket_name = "cjtantay.com"
 }
 
 resource "aws_s3_bucket" "cjtantay-dot-com" {
   bucket = local.bucket_name
-
-  lifecycle {
-    ignore_changes = [bucket]
-  }
 }
 
 resource "aws_s3_bucket_acl" "cjtantay-dot-com" {
