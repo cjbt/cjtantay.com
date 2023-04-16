32523a6c74d0f2f73c9dd14a1e758b59fa55f1f6
Cecil Tantay <cjbtantay@gmail.com>
2023-04-15 21:13:38 -0700

    update variable

--- tf/variables.tf
+++ tf/variables.tf
@@ -1,9 +1,9 @@
-variable "aws_access_key_id" {
+variable "aws_access_key" {
   type        = string
   description = "Deploy user key id."
 }
 
-variable "aws_secret_access_key" {
+variable "aws_secret_key" {
   type        = string
   description = "Deploy user key."
 }
