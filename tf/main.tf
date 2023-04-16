3150452574d7b3f805418ab1de914319d7b466e2
Cecil Tantay <cjbtantay@gmail.com>
2023-04-16 02:48:42 -0700

    update terraform

--- tf/main.tf
+++ tf/main.tf
@@ -6,20 +6,21 @@ provider "aws" {
 }
 
 locals {
-  bucket_name = "cjtantay.com"
+  sub_domain = "www.cjtantay.com"
+  root_domain = "cjtantay.com"
 }
 
-resource "aws_s3_bucket" "cjtantay-dot-com" {
-  bucket = local.bucket_name
+resource "aws_s3_bucket" "sub_domain" {
+  bucket = local.sub_domain
 }
 
-resource "aws_s3_bucket_acl" "cjtantay-dot-com" {
-  bucket = aws_s3_bucket.cjtantay-dot-com.id
+resource "aws_s3_bucket_acl" "sub_domain" {
+  bucket = aws_s3_bucket.sub_domain.id
   acl    = "private"
 }
 
-resource "aws_s3_bucket_website_configuration" "cjtantay-dot-com" {
-  bucket = aws_s3_bucket.cjtantay-dot-com.id
+resource "aws_s3_bucket_website_configuration" "sub_domain" {
+  bucket = aws_s3_bucket.sub_domain.id
 
   index_document {
     suffix = "index.html"
@@ -27,5 +28,23 @@ resource "aws_s3_bucket_website_configuration" "cjtantay-dot-com" {
 
   error_document {
     key = "index.html"
+  }
+}
+
+resource "aws_s3_bucket" "root_domain" {
+  bucket = local.root_domain
+}
+
+resource "aws_s3_bucket_acl" "root_domain" {
+  bucket = aws_s3_bucket.root_domain.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_website_configuration" "root_domain" {
+  bucket = aws_s3_bucket.root_domain.id
+
+  redirect_all_requests_to {
+    host_name = local.sub_domain
+    protocol  = "https"
   }
 }
